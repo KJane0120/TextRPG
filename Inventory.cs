c9480931284d0400a64282fb17bdf5bf79125926
KJane0120 <rlawoals5755@gmail.com>
2025-02-05 14:49:57 +0900

    Update Inventory.cs
    ItemList();함수를 호출하는게 아닌 for문을 이용해 직접 출력해주는 방식으로 수정
    장착 관리 메서드 구현 후 num ==1일때 장착 관리로 이동하게끔 연결

--- Inventory.cs
+++ Inventory.cs
@@ -7,9 +7,10 @@ using System.Threading.Tasks;
 namespace TextRPG
 {
 
-    internal class Inventory
+    internal class Inventory : Item
     {
-        Item item = new Item();
+
+
 
 
         public void OpenInventory()
@@ -17,21 +18,22 @@ namespace TextRPG
             Console.WriteLine();
             Console.WriteLine("인벤토리");
             Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
-            if (item != null)
-            {
-                item.ShowMyItem();
-            }
-            else
-            {
-                Console.Write("[아이템 목록]\n ");
-            }
+            //굳이 필요 없는 코드
+            //if (mine != null)
+            //{
+            //    mine.MyItemList();
+            //}
+            //else
+            //{
+            //    Console.Write("[아이템 목록] ");
+            //}
             Console.WriteLine();
             Console.WriteLine("1. 장착 관리");
             Console.WriteLine("0. 나가기");
             Console.WriteLine();
             Console.WriteLine("원하시는 행동을 입력해주세요");
             Console.Write(">> ");
-            
+
 
 
             while (true)
@@ -47,12 +49,14 @@ namespace TextRPG
                     if (num == 1)
                     {
                         Console.WriteLine("장착 관리로 이동합니다.");
+                        EquipItem();
+                        return;
+
                     }
                     else if (num == 0)
                     {
                         Console.WriteLine("메인 화면으로 이동합니다.");
-                        StartGame startgame = new StartGame();
-                        startgame.Start();
+                        StartGame.Start();
                         return;
                     }
 
@@ -67,6 +71,58 @@ namespace TextRPG
 
         }
 
+        public void EquipItem()
+        {
+            Console.WriteLine();
+            Console.WriteLine("인벤토리 - 장착 관리");
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+            Console.WriteLine();
+            //ItemList();
+            for (int i = 0; i < mine.myItems.Count; i++)
+            {
+                Console.Write($"- ");
+                items[i].ShowItemInfo(true);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            Console.Write(">> ");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine();
+                bool isValid = int.TryParse(input, out int num) && (num == 1 || num == 2 || num == 3 || num == 4 || num == 5 || num == 6 || num == 0);
+                if (isValid)
+                {
+                    if (num == 0)
+                    {
+                        Console.WriteLine("인벤토리로 돌아갑니다.");
+                        OpenInventory();
+                        return;
+                    }
+                    else 
+                    { //해제 기능 추가해야 함
+                        Console.WriteLine("아이템을 장착했습니다.");
+                        Item.isEquiped = true;
+                        Console.WriteLine();
+                        Console.WriteLine("원하시는 행동을 입력해주세요.");
+                        Console.Write(">> ");
+                        continue;
+                    }
+                }
+                //아이템에 없는 경우 장착 불가능 처리를 못함
+                else if(!isValid || num > mine.myItems.Count)
+                {
+                    Console.WriteLine("잘못된 입력입니다.");
+                    Console.WriteLine();
+                    Console.WriteLine("원하시는 행동을 입력해주세요.");
+                    Console.Write(">> ");
+                    continue;
+                }
+            }
+        }
 
 
     }
